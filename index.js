d422fe7b0e41a93eabe0b3c4f5894e113d8dbf9c
Ryan O'Toole <otoolerj@gmail.com>
2018-02-12 15:30:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var Object = new Object ()
+  var recipes = new Object ()
   delete new Object().key
   new Object();
 }
