8d2cd62cfb9ecb5f6f664f87b1e5254a7e029d1a
Ryan O'Toole <otoolerj@gmail.com>
2018-02-12 14:56:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-describe('Objects', function() {
-  it('defines a `recipes` object', function() {
-    expect(typeof recipes).toEqual('object')
-  })
 
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, obj, { [key]: value });
